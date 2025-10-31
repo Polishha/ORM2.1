d9f53b9d7776ee58ebeadafdea50750ecc87ac3b
Polisha <polina_marina00@bk.ru>
2025-11-01 02:08:10 +0300

    change

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+
+def article_list(request):
+    articles = Article.objects.all().prefetch_related('scopes__tag')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(
+        Article.objects.prefetch_related('scopes__tag'),
+        id=article_id
+    )
+
+    scopes = article.scopes.select_related('tag').all()
+    main_scope = None
+    other_scopes = []
+
+    for scope in scopes:
+        if scope.is_main:
+            main_scope = scope
+        else:
+            other_scopes.append(scope)
+
+    other_scopes.sort(key=lambda x: x.tag.name)
+
+    sorted_scopes = []
+    if main_scope:
+        sorted_scopes.append(main_scope)
+    sorted_scopes.extend(other_scopes)
+
+    context = {
+        'article': article,
+        'scopes': sorted_scopes
+    }
+    return render(request, 'articles/article_detail.html', context)
